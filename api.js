8ef292b8a8b5e948b4f016e1307c06de581f44b6
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2025-02-25 21:39:51 +0000

    implementing finally

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 const express = require('express');
-import { register } from './lib/register';
 
 const api = express()
 
@@ -7,4 +6,34 @@ const api = express()
 api.post('/register', async (req, res) => {
     const user = await req.json();
     register(user.username, user.password)
+})
+
+// Temporary Get requests for testing
+api.get('/register', (req, res) => {
+    const username = req.query.username;
+    const email = req.query.email;
+    const password = req.query.password;
+    const phone = req.query.phonenum;
+    res.send(`Hello ${username}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}`)
+
+    const registerUser = require('./lib/register');
+    registerUser(username, email, password, phone);
+})
+
+api.get('/newFork', (req, res) => {
+    // const creatorID = req.query.id;
+    // const recipeName = req.query.rname;
+    // const recipeDesc = req.query.rdesc;
+    // const recipeMethod = req.query.rmethod;
+    const newFork = require('./lib/newFork');
+    // newFork(creatorID, recipeName, recipeDesc, recipeMethod)
+
+    // customer / Creator id
+    // forkid of the parent / id of the recipe that is being forked
+    // data 
+    newFork(3, 16, "test3", "test3", "test3", null, null, null);
+})
+
+api.listen(process.env.API_PORT, () => {
+    console.log(`API listening on port ${process.env.API_PORT}`)
 })
